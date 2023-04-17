2f01e273e911f47c9b11dda5931bdb99dca6ba0e
diep <ngocdiepdn213@gmail.com>
2023-04-17 10:07:22 +0700

    up b2

---
+++ src/pages/users/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <h1>Header</h1>
+        <h1>Body</h1>
+        <h1>Footer</h1>
+    </div>
+  )
+}
+
+export default Home
